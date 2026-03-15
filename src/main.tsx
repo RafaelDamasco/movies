b73a38e2e11a7645f128e280386708182bd56e5e
Rafael <rafaeldamasco_@hotmail.com>
2026-03-15 17:19:03 -0300

    Chore: integrate React Query for data management

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { createRoot } from "react-dom/client";
-import "./index.css";
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import "./index.css";
 import { routeTree } from './routeTree.gen';
 
-const router = createRouter({ routeTree })
+const queryClient = new QueryClient()
+
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient
+  },
+  defaultPreload: "intent",
+  defaultPreloadStaleTime: 0,
+  scrollRestoration: true,
+})
 
 declare module '@tanstack/react-router' {
   interface Register {
@@ -12,6 +23,8 @@ declare module '@tanstack/react-router' {
   }
 }
 
-createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
-)
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />,
+  </QueryClientProvider>
+);
--- src/pages/__root.tsx
+++ src/pages/__root.tsx
@@ -1,13 +1,27 @@
-import { createRootRoute, HeadContent, Outlet } from '@tanstack/react-router'
+import type { QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { createRootRouteWithContext, HeadContent, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-
 
 const RootLayout = () => (
   <>
     <HeadContent />
     <Outlet />
     <TanStackRouterDevtools />
+    <ReactQueryDevtools />
   </>
 )
 
-export const Route = createRootRoute({ component: RootLayout })
+
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient
+}>()({
+  component: RootLayout,
+  notFoundComponent: () => {
+    return (
+      <div>
+        <p>This is the notFoundComponent configured on root route</p>
+      </div>
+    )
+  },
+})
